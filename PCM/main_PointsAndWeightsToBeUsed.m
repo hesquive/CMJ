5d0de3b71bf1e5b2d54a75712e1c3e74e1c085b6
Hugo Esquivel <hesquive@purdue.edu>
2021-08-19 11:47:42 -0500

    updating files...

--- PCM/main_PointsAndWeightsToBeUsed.m
+++ PCM/main_PointsAndWeightsToBeUsed.m
@@ -76,22 +76,25 @@ for i5=1:numPoints1
 end
 
 % now let's ignore those tiny weights that do not contribute significantly to the computation of the mean and variance:
-tol=1e-10; % any weight less than this tolerance will be ignored
+if strcmpi(probabilityDistribution,'Uniform')
+    tol=1e-10; % any weight less than this tolerance will be ignored
+    
+elseif strcmpi(probabilityDistribution,'Beta')
+    tol=1e-10; % any weight less than this tolerance will be ignored
+    
+elseif strcmpi(probabilityDistribution,'Normal')
+    tol=1e-10; % any weight less than this tolerance will be ignored
+end
 idx=y>tol; % points to be saved are denoted with 1
 
 % thus, realizations to be used for seismic simulations:
-p=z(idx,:); % realizations (note that they are normalized wrt the standard normal distribution^)
+p=z(idx,:); % realizations (note that they are normalized)
 v=y(idx); % corresponding weights
 
 fprintf('Number of points to be used: %d (instead of %d points).\n',length(v),length(y))
 fprintf('Thus, computational cost is reduced by %.1f%%.\n',(1-length(v)/length(y))*100)
 fprintf('Precision of resulting weights is %.14f%%.\n',sum(v)*100)
 fprintf('\n')
-
-% Footnote:
-% ^ to denormalize, take each column of p, say the ith column p(:,i), and perform the following operation:
-%        ith_sigma * p(:,i) + ith_mu,
-%   where ith_sigma and ith_mu are the standard deviation and the mean of the ith random variable, respectively.
 % ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -118,26 +121,28 @@ fclose(fid);
 % ----------------------------------------------------------------------------------------------------------------------
 % A CHECK:
 % ----------------------------------------------------------------------------------------------------------------------
-fprintf('Checking this approach with a polynomial function:\n')
-
-syms x1 x2 x3 x4 x5
-
-% analytical evaluation of mean and variance:
-fun=(x2+1)*(x3-2)*(x5-4)+(x1-1)^2*(x3+2)*(x4+1); % polynomial function
-pdf=(1/sqrt(2*pi))^5*exp(-1/2*(x1^2+x2^2+x3^2+x4^2+x5^2)); % joint pdf
-
-mean1=double(int(int(int(int(int(fun*pdf,x5,-Inf,Inf),x4,-Inf,Inf),x3,-Inf,Inf),x2,-Inf,Inf),x1,-Inf,Inf)); % exact mean
-var1=double(int(int(int(int(int((fun-mean1)^2*pdf,x5,-Inf,Inf),x4,-Inf,Inf),x3,-Inf,Inf),x2,-Inf,Inf),x1,-Inf,Inf)); % exact variance
-
-% numerical evaluation of mean and variance:
-funnum=matlabFunction(fun,'Vars',[x1,x2,x3,x4,x5]); % polynomial function
-pdfnum=matlabFunction(pdf,'Vars',[x1,x2,x3,x4,x5]); % joint pdf
-
-mean2=sum(funnum(p(:,1),p(:,2),p(:,3),p(:,4),p(:,5)).*v); % approximate mean
-var2=sum((funnum(p(:,1),p(:,2),p(:,3),p(:,4),p(:,5))-mean2).^2.*v); % approximate variance
-
-fprintf('- Exact solution:       [%f, %f].\n',mean1,var1)
-fprintf('- Approximate solution: [%f, %f].\n',mean2,var2)
-fprintf('- Error:                [%f%%, %f%%].\n',abs(1-mean2/mean1)*100,abs(1-var2/var1)*100)
-fprintf('\n')
+if strcmpi(probabilityDistribution,'Normal')
+    fprintf('Checking this approach with a polynomial function:\n')
+    
+    syms x1 x2 x3 x4 x5
+    
+    % analytical evaluation of mean and variance:
+    fun=(x2+1)*(x3-2)*(x5-4)+(x1-1)^2*(x3+2)*(x4+1); % polynomial function
+    pdf=(1/sqrt(2*pi))^5*exp(-1/2*(x1^2+x2^2+x3^2+x4^2+x5^2)); % joint pdf
+    
+    mean1=double(int(int(int(int(int(fun*pdf,x5,-Inf,Inf),x4,-Inf,Inf),x3,-Inf,Inf),x2,-Inf,Inf),x1,-Inf,Inf)); % exact mean
+    var1=double(int(int(int(int(int((fun-mean1)^2*pdf,x5,-Inf,Inf),x4,-Inf,Inf),x3,-Inf,Inf),x2,-Inf,Inf),x1,-Inf,Inf)); % exact variance
+    
+    % numerical evaluation of mean and variance:
+    funnum=matlabFunction(fun,'Vars',[x1,x2,x3,x4,x5]); % polynomial function
+    pdfnum=matlabFunction(pdf,'Vars',[x1,x2,x3,x4,x5]); % joint pdf
+    
+    mean2=sum(funnum(p(:,1),p(:,2),p(:,3),p(:,4),p(:,5)).*v); % approximate mean
+    var2=sum((funnum(p(:,1),p(:,2),p(:,3),p(:,4),p(:,5))-mean2).^2.*v); % approximate variance
+    
+    fprintf('- Exact solution:       [%f, %f].\n',mean1,var1)
+    fprintf('- Approximate solution: [%f, %f].\n',mean2,var2)
+    fprintf('- Error:                [%f%%, %f%%].\n',abs(1-mean2/mean1)*100,abs(1-var2/var1)*100)
+    fprintf('\n')
+end
 % ----------------------------------------------------------------------------------------------------------------------
